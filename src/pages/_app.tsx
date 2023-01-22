69dcbba054d6b84263dd8ca5a28ff532a97feeee
AkashSDas <aakashdas368@gmail.com>
2023-01-22 12:07:01 +0530

    add typography

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import "@/styles/globals.css";
 
-import { chakraTheme } from "@/lib/chakra-ui";
+import { chakraTheme, cubano, gtWalsheimPro } from "@/lib/chakra-ui";
 import { ChakraProvider } from "@chakra-ui/react";
 
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={chakraTheme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <div className={`${gtWalsheimPro.variable} ${cubano.variable}`}>
+      <ChakraProvider theme={chakraTheme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </div>
   );
 }
