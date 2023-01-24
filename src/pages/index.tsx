2b97a8c6b53ee761179703410ea65f1b9ec7a67a
AkashSDas <aakashdas368@gmail.com>
2023-01-24 20:07:35 +0530

    change head items

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import Articles from "@/components/articles";
 import ContactMe from "@/components/contact-me";
 import Experience from "@/components/experience";
@@ -10,6 +12,14 @@ import { Box } from "@chakra-ui/react";
 export default function HomePage() {
   return (
     <Box mt={pxToRem(56)}>
+      <Head>
+        <title>AkashSDas</title>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>📦</text></svg>"
+        />
+      </Head>
+
       <Hero />
       <Experience />
       <FeaturedProjects />
